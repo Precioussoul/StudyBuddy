e202c3c7cd9d5436476dac37cef4223e81c512cb
Precioussoul <habsoff@gmail.com>
2024-01-27 22:28:49 +0100

    feat: managing dynamic data

--- base/views.py
+++ base/views.py
@@ -3,7 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
+rooms = [    
+    {"id":1, "name":'lets learn python'},
+    {'id':2, "name":'Design with me'},
+    {'id':3, "name":'Backend developer'}
+]
+
 def home(request):
-    return  render(request, 'home.html')
-def room(request):
-    return  render(request, 'room.html')
+    context = {'rooms':rooms}
+    return  render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for item in rooms:
+         if item['id'] == int(pk):
+           room = item
+    
+    context = {'room':room}       
+    return  render(request, 'base/room.html', context)
