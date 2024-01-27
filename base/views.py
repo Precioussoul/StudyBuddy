1ac9395cf56c768e2c3377b9c94915e74155aab5
Precioussoul <habsoff@gmail.com>
2024-01-27 19:47:53 +0100

    feat: extending templates

--- base/views.py
+++ base/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return  HttpResponse('Home page')
+    return  render(request, 'home.html')
+def room(request):
+    return  render(request, 'room.html')
