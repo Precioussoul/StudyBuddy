54d8021eabd6b89b970bc1534791bba0ce39c4d5
Precioussoul <habsoff@gmail.com>
2024-01-28 18:48:39 +0100

    feat: working with database in Djamgo

--- base/views.py
+++ base/views.py
@@ -2,24 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Room
 
 
-rooms = [    
-    {"id":1, "name":'lets learn python'},
-    {'id':2, "name":'Design with me'},
-    {'id':3, "name":'Backend developer'}
-]
 
 def home(request):
+    rooms= Room.objects.all()
     context = {'rooms':rooms}
     return  render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for item in rooms:
-         if item['id'] == int(pk):
-           room = item
-    
+    room = Room.objects.get(id=pk)
     context = {'room':room}       
     return  render(request, 'base/room.html', context)
